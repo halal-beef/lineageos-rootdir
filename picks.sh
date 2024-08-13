a77b7c063aee7a9c4abb5f08227326966a1dc280
halal-beef <umer.uddin@mentallysanemainliners.org>
2024-08-13 22:27:18 +0100

    retire every other pick i dont need lol

--- picks.sh
+++ picks.sh
@@ -17,26 +17,7 @@ source build/envsetup.sh
 
 # -------------- PLATFORM STUFF --------------
 
-# build
-repopick -f 369796 # Sorry bro: 6 -> 3
-
-# frameworks/base
-repopick -f -P frameworks/base 385512 # BiometricScheduler: Cancel operation if not idle
-repopick -f -P frameworks/base 369786 # Alter model name to avoid SafetyNet HW attestation enforcement
-repopick -f -P frameworks/base 369787 # keystore: Block key attestation for SafetyNet
-repopick -f -P frameworks/base 369788 # AttestationHooks: Set shipping level to 32 for devices >=33
-repopick -f -P frameworks/base 369789 # Limit SafetyNet workarounds to unstable GMS process
-repopick -f -P frameworks/base 369790 # gmscompat: Apply the SafetyNet workaround to Play Store aswell
-repopick -f -P frameworks/base 369791 # gmscompat: Use Nexus 6P fingerprint for CTS/Integrity
-repopick -f -P frameworks/base 369792 # gmscompat: Make CTS/Play Integrity pass again
-repopick -f -P frameworks/base 374392 # gmscompat: Use new info
-repopick -f -P frameworks/base 357510 # gmscompat: also spoof props for samsung/sec apps
-
 # hardware/samsung_slsi-linaro/graphics
 repopick 384848 # WIP: libhwc2.1: invalidate last frame before changing display mode
-
-# vendor/lineage
-repopick -f -P vendor/lineage 381475 # SystemUI: More notification icons on AOD screen
-repopick -f -P vendor/lineage 369794 # lineage: Disable privapp permission enforcement (make it log)
 
 exit 0
