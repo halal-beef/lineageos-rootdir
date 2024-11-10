20c6d4d909a32b80a71c9c052e861e543b6b8b53
halal-beef <umer.uddin@mentallysanemainliners.org>
2024-11-10 19:45:12 +0000

    Mitigate mic issues

--- picks.sh
+++ picks.sh
@@ -17,6 +17,11 @@ source build/envsetup.sh
 
 # -------------- PLATFORM STUFF --------------
 
+# hardware/samsung_slsi-linaro/exynos
+repopick 399491 # audiohal_comv1: return BT SCO device when an actual BT SCO device is used
+repopick 399492 # exynos: audiohal_comv1: Always allow to reconfigure stream if voice  recognition is active
+repopick 399493 # exynos: audiohal_comv1: Ensure AUDIO_INPUT_FLAG_FAST is set when using low latency capture samplerate during calls
+
 # hardware/samsung_slsi-linaro/graphics
 repopick 384848 # WIP: libhwc2.1: invalidate last frame before changing display mode
 
